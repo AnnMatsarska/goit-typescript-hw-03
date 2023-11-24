43ffd5dbbf8e187b5e4ce24a6d7d929a83f95a21
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-11-24 19:29:44 +0200

    Update 4.ts

--- src/4.ts
+++ src/4.ts
@@ -15,8 +15,8 @@ class Person {
   }
 }
 abstract class House {
-  protected door: boolean;
-  protected tenants: Person[];
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
 
   constructor(protected key: Key) {}
 
